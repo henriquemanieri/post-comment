5458d52bc4c661bd39a4cd6d58cb44ae498943ad
Henrique Many <henrique.manieri@hotmail.com>
2022-11-29 16:24:34 -0500

    First comit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import styles from './Comment.module.css'
+
+import { ThumbsUp, Trash } from "phosphor-react"
+import { Avatar } from './Avatar'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/henriquemanieri.png" />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Kevin Malone</strong>
+                            <time title="11 de Maio ãs 04:20" dateTime="2022-08-20">Publicado a 1h</time>
+                        </div>
+                        <button title="Deletar Comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+
+            </div>
+        </div>
+
+    )
+}
